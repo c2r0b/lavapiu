3be6e63f7bc8d5ce1604ea5eba21abf5eb316291
c2r0b <c2r0b@proton.me>
2023-12-03 19:40:44 +0100

    csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,62 @@
 import createMiddleware from 'next-intl/middleware'
+import { NextResponse } from 'next/server'
 import { locales } from './i18n/config'
- 
+
+export function middleware(request:any) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, ' ')
+    .trim()
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+  requestHeaders.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+  response.headers.set(
+    'Content-Security-Policy',
+    contentSecurityPolicyHeaderValue
+  )
+
+  return response
+}
+
 export default createMiddleware({
   locales,
   defaultLocale: 'it'
 })
  
 export const config = {
-  matcher: ['/', '/(it)/:path*']
+  matcher: [
+    '/',
+    '/(it)/:path*',
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ]
 }
--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = ({ children }: { children: React.ReactNode }) => {
     return (
-        <footer className="flex bg-white dark:bg-black flex-col items-center justify-center w-full h-24 border-t text-gray-500 dark:border-neutral-700">
+        <footer className="flex bg-white dark:bg-black flex-col items-center justify-center w-full h-24 border-t">
             {children}
         </footer>
     )

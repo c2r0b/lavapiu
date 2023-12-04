bf0cf5c86ac08a1f5393dbb2de508035ee731c99
c2r0b <c2r0b@proton.me>
2023-12-04 11:22:38 +0100

    images fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,14 @@ export function middleware(request:any) {
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval' 'unsafe-inline' http: https:;
     style-src 'self' 'nonce-${nonce}' 'unsafe-hashes';
+    img-src 'self' blob: data: http: https:;
+    font-src 'self' http: https:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
 `
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
@@ -45,6 +53,8 @@ export function middleware(request:any) {
  
 export const config = {
   matcher: [
+    '/',
+    '/(it)/:path',
     {
       source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
       missing: [

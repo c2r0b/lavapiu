2e2c0915bc28a0eeec3f3860f82b8855e7a3206d
c2r0b <c2r0b@proton.me>
2024-04-08 16:03:05 +0200

    gallery images

--- src/app/[locale]/Gallery.tsx
+++ src/app/[locale]/Gallery.tsx
@@ -1,32 +1,67 @@
-import Image from 'next/image'
-import { useTranslations } from 'next-intl'
-import { Section } from '@/ui/Section'
-import { SectionTitle } from '@/ui/SectionTitle'
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { Section } from "@/ui/Section";
+import { SectionTitle } from "@/ui/SectionTitle";
+
+const galleryImages: galleryImage[] = [
+  {
+    key: "gallery-5",
+    src: "/images/gallery/5.webp",
+    alt: "Gallery image 5",
+    width: 800,
+    height: 600,
+  },
+  {
+    key: "gallery-6",
+    src: "/images/gallery/6.webp",
+    alt: "Gallery image 6",
+    width: 800,
+    height: 600,
+  },
+  {
+    key: "gallery-3",
+    src: "/images/gallery/3.webp",
+    alt: "Gallery image 3",
+    width: 800,
+    height: 600,
+  },
+  {
+    key: "gallery-1",
+    src: "/images/gallery/1.webp",
+    alt: "Gallery image 1",
+    width: 800,
+    height: 600,
+  },
+  {
+    key: "gallery-2",
+    src: "/images/gallery/2.webp",
+    alt: "Gallery image 2",
+    width: 800,
+    height: 600,
+  },
+];
 
 const Gallery = () => {
-    const t = useTranslations('gallery');
-    const galleryImages:galleryImage[] = [];
+  const t = useTranslations("gallery");
 
-    return (
-        <Section id="gallery" type="dark">
-            <SectionTitle>
-                {t('title')}
-            </SectionTitle>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            { galleryImages.map(image => (
-                <Image
-                    key={ image.key }
-                    className="relative border"
-                    src={ image.src }
-                    alt={ image.alt}
-                    width={ image.width }
-                    height={ image.height }
-                    priority
-                />
-            ))}
-            </div>
-        </Section>
-    )
-}
+  return (
+    <Section id="gallery" type="dark">
+      <SectionTitle>{t("title")}</SectionTitle>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {galleryImages.map((image) => (
+          <Image
+            key={image.key}
+            className="relative"
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+            priority
+          />
+        ))}
+      </div>
+    </Section>
+  );
+};
 
-export default Gallery
+export default Gallery;
